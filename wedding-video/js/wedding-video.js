bd943fb03e50ff869176614c0b52ae6b73d70ed5
Patrick Wilver <pwilver@hubspot.com>
2015-11-21 00:14:33 -0500

    v1 of wedding video page, issue with video width on mobile

--- wedding-video/js/wedding-video.js
+++ wedding-video/js/wedding-video.js
@@ -1,41 +1,35 @@
 var videos = {
 	init: function() {
 		this.eventBindings();
-		this.hideVideos();
 
 		var hash = window.location.href.split('#')[1];
 
 		if (hash) {
 			this.showVideo(hash);
-		} else {
-			this.showVideo('ceremony');
 		}
 	},
 
-	eventBindings: function() {
-		$('.video__nav a').click(function() {
-			var name = $(this).attr('data-name');
+	vimeoUrls: {
+		'ceremony': 'https://player.vimeo.com/video/146158163',
+		'cocktail-hour': 'https://player.vimeo.com/video/146161762',
+		'reception': 'https://player.vimeo.com/video/146155089'
+	},
 
-			videos.hideVideos();
+	eventBindings: function() {
+		$('.videos__nav a').click(function() {
+			var name = $(this).attr('data-name');
 			videos.showVideo(name);
 		});
 	},
 
-	hideVideos: function() {
-		$('.video').removeClass('show');
-	},
-
 	showVideo: function(id) {
-		if ($('#' + id).length > 0) {
-			$('#' + id).addClass('show');
+		if (videos.vimeoUrls[id]) {
+			$('iframe').attr('src', videos.vimeoUrls[id]);
 		} else {			
 			// remove bad anchor, reset url
 			var url = window.location.href.split('#')[0];
 			var state = { originalUrl: url };
 			window.history.replaceState(state, '', url);
-
-			// show default video
-			$('#ceremony').addClass('show');
 		}
 	}
 }
