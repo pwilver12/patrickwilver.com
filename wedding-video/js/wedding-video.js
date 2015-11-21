38f9ed2ff7970ffb20dbb673470dc424b6680936
Patrick Wilver <pwilver@hubspot.com>
2015-11-20 22:05:43 -0500

    Add proper click handlers to nav links

--- wedding-video/js/wedding-video.js
+++ wedding-video/js/wedding-video.js
@@ -4,15 +4,15 @@ var videos = {
 	},
 
 	eventBindings: function() {
-		$().click(function() {
-			
+		$('.video__nav a').click(function() {
+			var name = $(this).attr('data-name');
+			console.log(name);
+
+			videos.hideVideos();
+			videos.showVideo(name);
 		});
 	}
 }
-
-
-
-
 
 $(function() {
 	videos.init();
