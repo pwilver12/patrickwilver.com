5c492e621b2f1ca5ec231329af32e34b108d037d
Patrick Wilver <pwilver@hubspot.com>
2015-11-20 21:50:35 -0500

    Add wedding video js file to directory

---
+++ wedding-video/js/wedding-video.js
@@ -0,0 +1,19 @@
+var videos = {
+	init: function() {
+		this.eventBindings();
+	},
+
+	eventBindings: function() {
+		$().click(function() {
+			
+		});
+	}
+}
+
+
+
+
+
+$(function() {
+	videos.init();
+});
