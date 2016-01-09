4215538c402d30c80ea9c132e5596fe8482b4c98
Patrick Wilver <pwilver@hubspot.com>
2016-01-09 16:51:03 -0500

    v1 script for generating random lottery numbers on CTA click

---
+++ lottery/lottery.js
@@ -0,0 +1,41 @@
+var app = {
+	init: function() {
+		this.eventBindings();
+	},
+
+	eventBindings: function() {
+		$('.cta').click(function(e) {
+			e.preventDefault();
+			app.compileSelections();
+		});
+	},
+
+	selections: {},
+
+	compileSelections: function() {
+		for (i = 0; i < 5; i++) {
+			app.generateNumberUpTo(69, i);
+		}
+
+		app.generateNumberUpTo(26, 'power');
+	},
+
+	generateNumberUpTo: function(maxValue, index) {
+		var choice = Math.ceil(Math.random() * maxValue);
+		app.selections['ball_' + index] = choice;
+
+		app.displayNumber(index);
+	},
+
+	displayNumber: function(i) {
+		if (i === 'power') {
+			$('.' + i).html(app.selections['ball_' + i]);
+		} else {
+			$('.number')[i].html(app.selections['ball_' + i]);
+		}
+	}
+}
+
+$(function() {
+	app.init();
+});
